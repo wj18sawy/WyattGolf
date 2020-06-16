c35a724fd343d1fcb878a6ee7eb300798e9ea7a2
Wyatt Sawyer <thesoya@gmail.com>
2020-06-16 09:12:24 -0400

    opening prompts

--- main.py
+++ main.py
@@ -12,8 +12,27 @@ def save_bag(file_name):
     with open(file_name, "w") as write_file:
         json.dump(my_bag.__dict__, write_file)
 
+options = ["A", "B", "C"]
 
-open_bag('WyattGolf/WyattsBag.json')
-my_bag.name = "newName2"
-save_bag('WyattGolf/WyattsBag.json')
+print("Welcome to WyattGolf, open to new name suggestions...")
+while True:
+    print("Options (type letter of option and press enter) :")
+    print("[A] Play Golf")
+    print("[B] Range Session")
+    print("[C] Edit Bag")
+    choice = input("What would you like to do? : ")
+    if(choice in options):
+        break
+    else:
+        print(choice + " is not a valid option. Please select one of the following options")
+        print(*options, sep = ", ")
+
+
+
+# # Open bag and load to variable my_bag
+# open_bag('WyattGolf/WyattsBag.json')
+
+
+# # Save bag to file
+# save_bag('WyattGolf/WyattsBag.json')
 
